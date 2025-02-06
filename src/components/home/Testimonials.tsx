8238c2d31e5d6b6592580617482f8aeb9fb8f17d
iCONNmtl <hebert.kevin34@gmail.com>
2025-02-06 09:15:25 -0500

    Avis

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -2,39 +2,39 @@ import React from 'react';
 
 const TESTIMONIALS = [
   {
-    name: 'Alice Johnson',
+    name: 'Alice Prigent',
     username: 'E-commerce Etsy',
-    avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar3.webp',
     content: "Franchement, super pratique ! En deux clics, je fais mes mockups et je peux les adapter direct pour Pinterest. Un vrai gain de temps !"
   },
   {
-    name: 'David Smith',
+    name: 'David Lopez',
     username: 'Community Manager',
-    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar5.webp',
     content: 'Je trouve toujours un mockup qui correspond à ce que je veux. C’est rapide, facile à utiliser et les résultats sont vraiment pros.'
   },
   {
     name: 'Emma Brown',
     username: 'Freelance',
-    avatar: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar4.webp',
     content: 'Le fait de pouvoir personnaliser avec du texte ou des images, c’est top. Et ça prend littéralement quelques secondes !'
   },
   {
     name: 'James Wilson',
     username: 'Designer Graphique',
-    avatar: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar1.webp',
     content: 'Générer plusieurs mockups d’un coup, ça change tout. Plus besoin de galérer, tout est prêt hyper vite.'
   },
   {
     name: 'Sophia Lee',
     username: 'Freelance',
-    avatar: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar6.webp',
     content: 'J’adore qu’il y ait des nouveautés régulièrement, on sent que l’outil s’adapte à ce qu’on attend. Hyper cool !'
   },
   {
     name: 'Michael Davis',
     username: 'E-commerce',
-    avatar: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80',
+    avatar: 'https://d2v7vpg8oce97p.cloudfront.net/Branding/Avatar2.webp',
     content: 'La qualité est dingue, et tout est optimisé pour les réseaux. Je ne peux plus m’en passer pour mes fiches produits !'
   }
 ];
