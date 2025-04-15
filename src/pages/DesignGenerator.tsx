c79419c5bb13dd670a8a17a2a16f6deee6aa2f67
iCONNmtl <hebert.kevin34@gmail.com>
2025-04-15 09:43:03 -0400

    OK

--- src/pages/DesignGenerator.tsx
+++ src/pages/DesignGenerator.tsx
@@ -877,7 +877,6 @@ export default function DesignGenerator() {
             onClick={() => setShowTemplateGallery(true)}
             className="flex items-center px-4 py-2 bg-indigo-100 text-indigo-700 rounded-lg hover:bg-indigo-200 transition-colors"
           >
-            <Template className="h-5 w-5 mr-2" />
             Templates
           </button>
           <button
